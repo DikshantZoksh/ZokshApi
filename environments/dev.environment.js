31819464cb157de8d04eb841bf3112bf9f04d120
Ashish Rana <dev.livelifelively@gmail.com>
2022-09-16 19:52:15 +0530

    script fixes

---
+++ environments/dev.environment.js
@@ -0,0 +1,15 @@
+const baseurl = "http://payments.moopay.local";
+const apiAuth = {
+  key: '627b4a2836b59026e4d5cd6c',
+  secret: 'sk_test_EM+cYhQ50tBO31PAVEifGw=='
+}
+const versions = {
+  versionTwo: 'v1',
+  versionOne: 'v2'
+}
+
+module.exports = {
+  baseurl,
+  apiAuth,
+  versions
+}
---
+++ environments/testnet.environment.js
@@ -0,0 +1,17 @@
+const baseurl = 'https://payments.testnet.zoksh.com';
+
+const apiAuth = {
+  key: '627b4a2836b59026e4d5cd6c',
+  secret: 'sk_test_EM+cYhQ50tBO31PAVEifGw=='
+}
+
+const versions = {
+  versionTwo: 'v1',
+  versionOne: 'v2'
+}
+
+module.exports = {
+  baseurl,
+  apiAuth,
+  versions
+}
