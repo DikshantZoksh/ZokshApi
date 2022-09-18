4b21270e0513a7eee44e49152f39ccfe9ebe4a81
Dikshant Agarwal <dikshant.agarwal@MACPRO380.local>
2022-09-18 17:20:25 +0530

    Added testdat and code for 5th scenario

--- test/orders.spec.js
+++ test/orders.spec.js
@@ -138,6 +138,33 @@ describe('Zoksh OrderCreation API Tests', () => {
       });
   }).timeout(10000);
 
+  it('TC-05 : Name,Phone,Amount - NA : No Specified chain or currency : Should successfully create a order', (done) => {
+    request(baseurl)
+      .post(requestapi.url)
+      .send(requestapi.body)
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .set('zoksh-key', zokshkey)
+      .set('zoksh-ts', zokshts)
+      .set('zoksh-sign', zokshsign)
+      .end(function (err, res) {
+        if (err) {
+          throw err;
+        }
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.body.orderId).to.be.not.null;
+        expect(res.body.url).to.be.not.null;
+        str_ordrId = res.body.orderId 
+        val_url = res.body.url
+        console.log(typeof(val_url))
+        console.log(typeof(str_ordrId))
+        //console.log(str_ordrId)
+        expect(res.body.url).contains(str_ordrId)
+        console.log(JSON.stringify(res.body));
+        done();
+      });
+  }).timeout(10000);
+
 
 
 
